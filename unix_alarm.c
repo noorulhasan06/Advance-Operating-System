18c2b35451ff357bcb5a0cd033707050477b0040
Hasan <NHasan@cvent.com>
2019-06-23 16:15:18 +0530

    change in file unix_alarm.c

--- unix_alarm.c
+++ unix_alarm.c
@@ -1,29 +1,32 @@
+/**
+ * Comment added.
+ */
 #include <sys/types.h>
-#include <syw/stat.h> 
-#include <syw/signal.h> 
+#include <syw/stat.h>
+#include <syw/signal.h>
 main (argc, argv) int argc; char *argv[]; {
-	extern unsigned alarm(); 
-	extern wakeup(); 
-	struct stat statbuf; 
-	time_t axtime; 
-	if (argc != 2)	{ 
-		printf("only 1 arg\n"); 
+	extern unsigned alarm();
+	extern wakeup();
+	struct stat statbuf;
+	time_t axtime;
+	if (argc != 2)	{
+		printf("only 1 arg\n");
 		exit();
-	}		
-	axtime = (time_t) 0; 
+	}
+	axtime = (time_t) 0;
 	for (;;) { /* find out file access time */
 		if (stat(argv[l], &statbuf) == -1) {
 			printf("file %s not there\n ", argv[1]);
-			exit(); 
+			exit();
 		}
 		if (axtime != statbuf.st_atime) {
 			printf("file %s accessed\n", argv[1]);
 			axtime = statbuf.st_atime;
-		}			
+		}
 		signal(SIGALRM, wakeup); /* reset for alarm */
-		alarm(60); 
+		alarm(60);
 		pause(); /* sleep until signal */
-		
+
 	}
-}	
-wakeup() ( } 
+}
+wakeup() ( }
